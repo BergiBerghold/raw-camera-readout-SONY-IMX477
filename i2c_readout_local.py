ebe150c5e9919b4a73d820dbb5d801b03d111aeb
Michael Berghold <michael.berghold@oewf.org>
2022-09-26 18:37:46 +0200

    .

--- i2c_readout_local.py
+++ i2c_readout_local.py
@@ -1,13 +1,14 @@
 import pigpio
 
 pi = pigpio.pi()
-h = pi.i2c_open(10, 0x10, 0)
+h = pi.i2c_open(10, 0x1A, 0)
 
-pi.i2c_write_device(h, [0x01, 0x40, 0x80])
+#pi.i2c_write_device(h, [0x01, 0x40, 0x80])
 
-pi.i2c_write_device(h, [0x01, 0x40])
-count, data = pi.i2c_read_device(h, 1)
+pi.i2c_write_device(h, [0x02, 0x00])
+count, data = pi.i2c_read_device(h, 2)
 
-print(int.from_bytes(data, 'big') - 128)
+for byte in data:
+    print(int.from_bytes(byte, 'big'))
 
 pi.i2c_close(h)
